5605d78cc670fc821b1244a59bcf75fe8b79008d
hlhuang <hlhuang@yangqianguan.com>
2019-02-24 23:14:37 +0800

    encoder_0.1

--- data_core.py
+++ data_core.py
@@ -1,28 +1,24 @@
 from hyperparams import Hyperparams as hp
-import os
 from collections import Counter
 import regex
 import numpy as np
 import tensorflow as tf
 
 
-
-
-
 def load_vocab(fname):
-	"""
+    """
 	加载词典
 	:param fname:
 	:return:
 	"""
-	vocab  = [line.split()[0] for line in open(fname,'r').readlines() if int(line.split()[1]) > hp.min_cnt]
-	word2idx = {word : idx for idx, word in enumerate(vocab)}
-	idx2word = {idx : word for idx, word in enumerate(vocab)}
-	return  word2idx,idx2word
+    vocab = [line.split()[0] for line in open(fname, 'r',encoding='utf-8').readlines() if int(line.split()[1]) > hp.min_cnt]
+    word2idx = {word: idx for idx, word in enumerate(vocab)}
+    idx2word = {idx: word for idx, word in enumerate(vocab)}
+    return word2idx, idx2word
 
 
 def make_vocab(fpath, fname):
-	'''
+    '''
 	处理原始数据,对词计数,然后按词频从大到小,每行保存。
 	单词 频率
 	the	155830
@@ -33,70 +29,75 @@ def make_vocab(fpath, fname):
 	:param fname: 输出文件
 	:return:
 	'''
-	with open(fpath, 'r') as f:
-		# words = [regex.sub("[^\s\p{Latin}']", "", line).split() for line in f.readlines()]
-		text = f.read()
-		text = regex.sub("[^\s\p{Latin}']", "", text)
-		words = text.split()
-		word2cnt = Counter(words)
-		with open(fname,'w') as fout:
-			fout.write("{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n".format("<PAD>", "<UNK>", "<S>","</S>"))
-			for word, cnt in word2cnt.most_common(len(word2cnt)):
-				fout.write("{}\t{}\n".format(word, cnt))
+    # 'r+', encoding = "utf-8"
+    with open(fpath, 'r+', encoding='utf-8') as f:
+        # words = [regex.sub("[^\s\p{Latin}']", "", line).split() for line in f.readlines()]
+        text = f.read()
+        text = regex.sub("[^\s\p{Latin}']", "", text)
+        words = text.split()
+        word2cnt = Counter(words)
+        with open(fname, 'w+',encoding='utf-8') as fout:
+            fout.write(
+                "{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n{}\t1000000000\n".format("<PAD>", "<UNK>", "<S>",
+                                                                                          "</S>"))
+            for word, cnt in word2cnt.most_common(len(word2cnt)):
+                fout.write("{}\t{}\n".format(word, cnt))
 
 
 def create_data(source_sent, target_sent):
-	de2idx, idx2de = load_vocab(hp.de_processed_fname)
-	en2idx, idx2en = load_vocab(hp.en_processed_fname)
-	x_list, y_list , Sources, Targets = [], [], [], []
-	for source_sent, target_sent in zip(source_sent, target_sent):
-		x = [de2idx.get(word, 1) for word in (source_sent + " </S>").split()]
-		y = [en2idx.get(word, 1) for word in (target_sent + " </S>").split()]
-		if max(len(x), len(y)) < hp.maxlen:
-			x_list.append(np.array(x))
-			y_list.append(np.array(y))
-			Sources.append(source_sent)
-			Targets.append(target_sent)
-	
-	X = np.zeros([len(x_list), hp.maxlen], np.int32)
-	Y = np.zeros([len(y_list), hp.maxlen], np.int32)
-	for i, (x, y) in enumerate(zip(x_list, y_list)):
-		X[i] = np.pad(x, (0, hp.maxlen - len(x)), 'constant', constant_values=(0, 0))
-		Y[i] = np.pad(y, (0, hp.maxlen - len(y)), 'constant', constant_values=(0, 0))
-	
-	return X, Y, Sources, Targets
+    de2idx, idx2de = load_vocab(hp.de_processed_fname)
+    en2idx, idx2en = load_vocab(hp.en_processed_fname)
+    x_list, y_list, Sources, Targets = [], [], [], []
+    for source_sent, target_sent in zip(source_sent, target_sent):
+        x = [de2idx.get(word, 1) for word in (source_sent + " </S>").split()]
+        y = [en2idx.get(word, 1) for word in (target_sent + " </S>").split()]
+        if max(len(x), len(y)) < hp.maxlen:
+            x_list.append(np.array(x))
+            y_list.append(np.array(y))
+            Sources.append(source_sent)
+            Targets.append(target_sent)
+
+    X = np.zeros([len(x_list), hp.maxlen], np.int32)
+    Y = np.zeros([len(y_list), hp.maxlen], np.int32)
+    for i, (x, y) in enumerate(zip(x_list, y_list)):
+        X[i] = np.pad(x, (0, hp.maxlen - len(x)), 'constant', constant_values=(0, 0))
+        Y[i] = np.pad(y, (0, hp.maxlen - len(y)), 'constant', constant_values=(0, 0))
+
+    return X, Y, Sources, Targets
 
 
 def load_train_data():
-	de_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in open(hp.source_train,'r').readlines() if line[0] !="<"]
-	en_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in open(hp.target_train, 'r').readlines() if line[0] !="<"]
-	
-	X, Y, Sources, Targets = create_data(de_sents, en_sents)
-	
-	return X, Y
+    de_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in open(hp.source_train, 'r', encoding='utf-8').readlines() if
+                line[0] != "<"]
+    en_sents = [regex.sub("[^\s\p{Latin}']", "", line) for line in open(hp.target_train, 'r', encoding='utf-8').readlines() if
+                line[0] != "<"]
+
+    X, Y, Sources, Targets = create_data(de_sents, en_sents)
+
+    return X, Y
+
 
 def get_batch_data():
-	# Load train data
-	X, Y = load_train_data()
-	num_batch = len(X) // hp.batch_size
-	
-	# convert to tensor
-	
-	X = tf.convert_to_tensor(X, tf.int32)
-	Y = tf.convert_to_tensor(Y, tf.int32)
-	
-	# create Queues
-	input_queues = tf.train.slice_input_producer([X, Y])
-	
-	# create batch queues
-	x, y = tf.train.shuffle_batch(input_queues, num_threads=8, batch_size=hp.batch_size, capacity=hp.batch_size*64,
-	                              min_after_dequeue=hp.batch_size*32,
-	                              allow_smaller_final_batch=False)
-	return x, y, num_batch
-	
-	
-if __name__  == '__main__':
-	make_vocab(hp.source_train, hp.de_processed_fname)
-	make_vocab(hp.target_train, hp.en_processed_fname)
-	print("Done")
-	
+    # Load train data
+    X, Y = load_train_data()
+    num_batch = len(X) // hp.batch_size
+
+    # convert to tensor
+
+    X = tf.convert_to_tensor(X, tf.int32)
+    Y = tf.convert_to_tensor(Y, tf.int32)
+
+    # create Queues
+    input_queues = tf.train.slice_input_producer([X, Y])
+
+    # create batch queues
+    x, y = tf.train.shuffle_batch(input_queues, num_threads=8, batch_size=hp.batch_size, capacity=hp.batch_size * 64,
+                                  min_after_dequeue=hp.batch_size * 32,
+                                  allow_smaller_final_batch=False)
+    return x, y, num_batch
+
+
+if __name__ == '__main__':
+    make_vocab(hp.source_train, hp.de_processed_fname)
+    make_vocab(hp.target_train, hp.en_processed_fname)
+    print("Done")
--- train.py
+++ train.py
@@ -1,46 +1,51 @@
-
-from data_core import load_vocab,get_batch_data
+from data_core import load_vocab, get_batch_data
 from hyperparams import Hyperparams as hp
 import tensorflow as tf
 from tqdm import tqdm
 
+
 class Graph():
-	def __init__(self,is_training = True):
-		self.graph = tf.Graph()
-		with self.graph.as_default():
-			if is_training:
-				self.x, self.y, self.num_batch = get_batch_data()  # (N, T)
-				# self.x, self.y, self.num_batch = None, None, None  # (N, T)
-			else:  # inference
-				self.x = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
-				self.y = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
-				
-			# define decoder inputs
-			self.decoder_inputs = tf.concat((tf.ones_like(self.y[:, :1]) * 2, self.y[:, :-1]), -1)
-		
-			de2idx, idx2de = load_vocab(hp.de_processed_fname)
-			en2idx, idx2en = load_vocab(hp.en_processed_fname)
-	
+    def __init__(self, is_training=True):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            if is_training:
+                self.x, self.y, self.num_batch = get_batch_data()  # (N, T)
+            # self.x, self.y, self.num_batch = None, None, None  # (N, T)
+            else:  # inference
+                self.x = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
+                self.y = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
+            # define decoder inputs
+            # self.y[:, :1].shape (32,1), self.y[:, :-1].shape (32,9)
+            # 这里没看懂为啥*2，
+            self.decoder_inputs = tf.concat((tf.ones_like(self.y[:, :1]) * 2, self.y[:, :-1]), -1)
+
+            de2idx, idx2de = load_vocab(hp.de_processed_fname)
+            en2idx, idx2en = load_vocab(hp.en_processed_fname)
+
+            # Encoder
+            with tf.variable_scope("encoder"):
+                ## Embedding
+                pass
 
 
 if __name__ == '__main__':
-	de2idx,idx2de = load_vocab(hp.de_processed_fname)
-	en2idx,idx2en = load_vocab(hp.en_processed_fname)
-	
-	# build graph
-	g = Graph()
-	print("Graph Loaded")
-	
-	# start session
-	sv = tf.train.Supervisor(graph=g.graph, logdir=hp.logdir, save_model_secs=0)
-	
-	with sv.managed_session() as sess:
-		for epoch in range(1, hp.num_epochs + 1):
-			if sv.should_stop(): break
-			for step in tqdm(range(g.num_batch), total = g.num_batch, ncols = 70, leave = False, unit = 'b'):
-				sess.run(g.decoder_inputs)
-				
-			# gs = sess.run(g.global_step)
-			sv.saver.save(sess, hp.logdir + '/model_epoch_%02d_gs_%d' % (epoch, gs))
-			
-	print("Done")
+    de2idx, idx2de = load_vocab(hp.de_processed_fname)
+    en2idx, idx2en = load_vocab(hp.en_processed_fname)
+
+    # build graph
+    g = Graph()
+    print("Graph Loaded")
+
+    # start session
+    sv = tf.train.Supervisor(graph=g.graph, logdir=hp.logdir, save_model_secs=0)
+
+    with sv.managed_session() as sess:
+        for epoch in range(1, hp.num_epochs + 1):
+            if sv.should_stop(): break
+            for step in tqdm(range(g.num_batch), total=g.num_batch, ncols=70, leave=False, unit='b'):
+                sess.run(g.decoder_inputs)
+
+            gs = sess.run(g.global_step)
+            sv.saver.save(sess, hp.logdir + '/model_epoch_%02d_gs_%d' % (epoch, gs))
+
+    print("Done")
