a6989fd5f6eeb1c0fa2032244d94365f1ee8626d
luolanfeixue <dnrhhl@gmail.com>
2019-02-25 21:01:06 +0800

    encoder-0.2

---
+++ modules.py
@@ -0,0 +1,82 @@
+import tensorflow as tf
+import numpy as np
+
+
+def embedding(inputs, vocab_size, num_units, zero_pad=True, scale=True,
+              scope="embedding", reuse=None):
+	"""
+	
+	:param inputs: 一个tensor，包含了词到id，以便于 lookup table
+	:param vocab_size: 词典大小
+	:param num_units: embeding 的 hidden units
+	:param zero_pad: boolean, 如果是true,则第一行全为0
+	:param scale: boolean,是否归一化，如果true，则全都除以 sqrt num_units
+	:param scope: variable_scope
+	:param reuse: boolean, 是否以相同的名字重新使用前一个layer的权重
+	:return:
+		被 embedding后的 tensor, 最后一个维度为num_units
+    >>
+    outputs.shape (2, 3, 9)
+	"""
+	
+	with tf.variable_scope(scope, reuse=reuse):
+		lookup_table = tf.get_variable('lookup_table', dtype=tf.float32, shape=[vocab_size, num_units],
+		                               initializer=tf.contrib.layers.xavier_initializer())
+		#  lookup_table的第一行强制设置为0
+		if zero_pad:
+			lookup_table = tf.concat((tf.zeros(shape=[1, num_units])), lookup_table[1:, :], 0)
+		# inputs.shape(32,10) lookup_table.shape=(vocab_size,512),outputs.shape(32,10,512)
+		outputs = tf.nn.embedding_lookup(lookup_table, inputs)
+		
+		if scale:
+			outputs = outputs * (num_units ** 0.5)
+	
+	return outputs
+
+
+# inputs = tf.to_int32(tf.reshape(tf.range(320), (32, 10)))
+# outputs = embedding(inputs, 100000, 512, zero_pad=False)
+# with tf.Session() as sess:
+# 	sess.run(tf.global_variables_initializer())
+# 	print(sess.run(outputs)) outputs.shape = (32,10,512)
+
+def positional_encoding(inputs, num_units, zero_pad=True, scale=True, scope="positional_encoding", reuse=None):
+	"""
+	:param inputs:  A 2d Tensor with shape of (N, T). N = batch_size, T = length of sequence
+	:param num_units:
+	:param zero_pad: boolean. If True, all the values of the first row ( id = 0) should be constant zero
+	:param scale: boolean.  If True, the output will be multiplied by sqrt num_units(check details from paper)
+	:param scope: Optional scope for `variable_scope`.
+	:param reuse:  Boolean, whether to reuse the weights of a previous laye  by the same name.
+	:return:  A 'Tensor' with one more rank than inputs's, with the dimensionality should be 'num_units'
+	"""
+	
+	N, T = inputs.get_shape().as_list() # N = 32, T = 10
+	with tf.variable_scope(scope, reuse=reuse):
+		position_ind = tf.tile(tf.expand_dims(tf.range(T), 0), [N, 1])
+		# position_enc.shape = (10,512)
+		position_enc = np.array([
+			[pos / np.power(10000, 2. * i / num_units) for i in range(num_units)]
+			for pos in range(T)])
+		# seconde part, apply the cosine to even colums ans sin to odds
+		position_enc[:, 0::2] = np.sin(position_enc[:, 0::2])  # dim 2i
+		position_enc[:, 1::2] = np.sin(position_enc[:, 1::2])  # dim 2i+1
+		
+		lookup_table = tf.convert_to_tensor(position_enc)
+		
+		if zero_pad:
+			lookup_table = tf.concat((tf.zeros(shape=[1, num_units])), lookup_table[1:, :], 0)
+		
+		outputs = tf.nn.embedding_lookup(lookup_table, position_ind)
+		
+		if scale:
+			outputs = outputs * (num_units ** 0.5)
+		return outputs
+#
+#
+# inputs = tf.to_int32(tf.reshape(tf.range(320), (32, 10)))
+# outputs = positional_encoding(inputs, 512, zero_pad=False)
+# with tf.Session() as sess:
+# 	sess.run(tf.global_variables_initializer())
+# 	print(sess.run(outputs)) outputs.shape (32,10,512)
+# 	pass
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from data_core import load_vocab, get_batch_data
 from hyperparams import Hyperparams as hp
 import tensorflow as tf
 from tqdm import tqdm
+from modules import *
 
 
 class Graph():
@@ -15,7 +16,7 @@ class Graph():
                 self.x = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
                 self.y = tf.placeholder(tf.int32, shape=(None, hp.maxlen))
             # define decoder inputs
-            # self.y[:, :1].shape (32,1), self.y[:, :-1].shape (32,9)
+            # self.y[:, :1].shape=(32,1), self.y[:, :-1].shape= (32,9) self.decoder_inputs.shape = (32, 10)
             # 这里没看懂为啥*2，
             self.decoder_inputs = tf.concat((tf.ones_like(self.y[:, :1]) * 2, self.y[:, :-1]), -1)
 
@@ -24,9 +25,13 @@ class Graph():
 
             # Encoder
             with tf.variable_scope("encoder"):
-                ## Embedding
+                ## Embedding self.enc.shape = (32, 10, 512)，给每个词编码。
+                self.enc = embedding(self.x, vocab_size = len(de2idx), num_units = hp.hidden_units, scale = True,
+                                     scope = 'enc_embed')
+                if hp.sinusoid:
+                    self.enc += positional_encoding(self.x, num_units = hp.hidden_units, zero_pad = False, scale = False,
+                                                    scope = 'enc_pe')
                 pass
-
 
 if __name__ == '__main__':
     de2idx, idx2de = load_vocab(hp.de_processed_fname)
